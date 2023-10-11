f540eb995696e28ed83b483a6479e307dee592fd
MrLad01 <ladayodeji@gmail.com>
2023-10-11 10:21:01 +0100

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,30 @@
 import { useState } from 'react'
+import image from "../src/assets/images/illustration-sign-up-desktop.svg"
 
 
 function App() {
-
+  const [submitted, setSubmitted] = useState("");
 
   return (
-    <>
-
-    </>
+    <main className='bg-slate-500 h-screen w-screen'>
+      {
+        !submitted     ? 
+      <div className='flex bg-slate-500 h-screen w-screen'>
+        <form action="post">
+          <h1>Stay updated!</h1>
+          <p>Join 60,000+ product managers receiving monthly updates on:</p>
+          <ul>
+            <li>Product discovery and building what matters</li>
+            <li> Measuring to ensure updates are a success</li>
+            <li>And much more!</li>
+          </ul>
+        </form>
+        <img src={image} alt="" />
+      </div>  : 
+      
+      <>
+      </>}
+    </main>
   )
 }
 
